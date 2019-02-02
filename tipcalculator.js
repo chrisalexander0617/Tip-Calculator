00ea5fc58732ae94e4dfaff554762ea4846f65ef
Chris <44784345+chrisalexander0617@users.noreply.github.com>
2019-02-02 14:54:37 -0500

    Add files via upload

--- tipcalculator.js
+++ tipcalculator.js
@@ -1,13 +1,22 @@
 
-//jQuery conversion 
 $('#button').click(function calculate(){
   console.log('creating variable for user input for text')
-  var bill = $("input[type='text']").val();
+  let bill = $("input[type='text']").val();
   console.log(Number(bill));
-  var numberOfPercent = $("input[type='number']").val();
+
+  function getSelectedValue()
+  {
+  let selectedValue = $("#dropdown").val();
+  let int = parseInt(selectedValue);
+
+  console.log(int);
   console.log('creating total');
-  var total = Number(bill) * (Number(numberOfPercent) / 100) + Number(bill);
-$('#result').text('$'+ total.toFixed(2))
-})
 
+  var total = Number(bill) * (Number(int) / 100) + Number(bill);
 
+  console.log("The total is " + total);
+
+  $('#result').text('$'+ total.toFixed(2));
+  }
+  getSelectedValue();
+});
